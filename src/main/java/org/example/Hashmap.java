3975e82156db7fda1a6a96be9c86571cac529e77
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-02-13 12:55:49 +0530

    updated

--- src/main/java/org/example/Hashmap.java
+++ src/main/java/org/example/Hashmap.java
@@ -15,7 +15,7 @@ public class Hashmap
         languages.put(1, "Java");
         languages.put(2, "Python");
         languages.put(3, "JavaScript");
-        l.info("HashMap: " + languages);
+        l.log(Level.INFO,()->"HashMap: " + languages);
         l.log(Level.INFO,()->"Keys: " + languages.keySet());//->return keys only
         l.log(Level.INFO,()->"Values: " + languages.values());//->return values only
         l.log(Level.INFO,()->"Key/Value mappings: " + languages.entrySet());//->return keys and values
@@ -23,13 +23,13 @@ public class Hashmap
         l.log(Level.INFO,()->"Removed value: " + value);
         l.log(Level.INFO,()->"Updated HashMap: " + languages);//->updated hashmap
         for (Integer key : languages.keySet()) {//->iteration with keys
-            l.info("The key:"+key);
+            l.log(Level.INFO,()->"The key:"+key);
         }
         for (String values : languages.values()) {//->iteration with values only
             l.info(values);
         }
         for (Entry<Integer, String> entry : languages.entrySet()) {//->iteration with keys and values pari
-            l.info("key/value"+entry);
+            l.log(Level.INFO,()->"key/value"+entry);
         }
     }
 }
